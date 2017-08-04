113d08e15557fe6bbc051e559eb7186169797c5c
kevin <kevin.lambert@devoteamgcloud.com>
2017-08-04 14:54:05 +0200

    Update setups

--- setup.py
+++ setup.py
@@ -5,8 +5,11 @@ Setup file describing how to install.
 from setuptools import setup
 
 setup(
-    name=u'modestfactory',    # This is the name of your PyPI-package.
-    version=u'0.1',                          # Update the version number for new releases
+    name=u'modestfactory',
+    version=u'0.1',
     packages=[u'modestfactory'],
-    install_requires=[]
+    install_requires=[],
+    url='https://github.com/duckswitch/modest-factory.git',
+    download_url='https://github.com/duckswitch/modest-factory.git/archive/0.1.tar.gz',
+    keywords=[]
 )
