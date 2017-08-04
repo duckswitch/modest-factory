c3d505292e517fa421f4ff43c32536412e3ccd96
kevin <kevin.lambert@devoteamgcloud.com>
2017-08-04 14:35:01 +0200

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+"""
+Setup file describing how to install.
+"""
+
+from setuptools import setup
+
+setup(
+    name=u'simplefactory',    # This is the name of your PyPI-package.
+    version=u'0.1',                          # Update the version number for new releases
+    packages=[u'simplefactory'],
+    install_requires=[]
+)
